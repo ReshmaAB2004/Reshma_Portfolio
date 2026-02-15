b78400f29384be15f6f63a6b54651d85ed880500
Reshma <reshmaab2004@gmail.com>
2026-02-15 12:39:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const menu = document.getElementById("mobile-menu");
+const navLinks = document.querySelector(".nav-links");
+
+menu.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+
